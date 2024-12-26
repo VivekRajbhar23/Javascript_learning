690fd196611312713ef6296d3fe18fa94f37e2e7
VivekRajbhar23 <114468572+VivekRajbhar23@users.noreply.github.com>
2024-12-26 10:26:38 +0000

    Conversion of datatype

--- .devcontainer/01_basics_of_js/test.js
+++ .devcontainer/01_basics_of_js/test.js
@@ -1,10 +1,42 @@
-const a=12345
-let b="vivek"
-var c="vik@gmail.com"
+// const a=12345
+// let b="vivek"
+// var c="vik@gmail.com"
 
-console.table([a,b,c])
+// console.table([a,b,c])
 
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
 */
+//---------------------------------------------------
+
+// let score = "Vivek"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "Vivek"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+
+//"" => false
+
+//"vivek" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
