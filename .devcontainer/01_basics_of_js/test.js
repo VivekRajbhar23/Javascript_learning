b1ab7b14d39543ec8a3cbfa6592d01ddafbac273
VivekRajbhar23 <114468572+VivekRajbhar23@users.noreply.github.com>
2024-12-26 05:51:53 +0000

    container config done

--- .devcontainer/01_basics_of_js/test.js
+++ .devcontainer/01_basics_of_js/test.js
@@ -1,1 +1,10 @@
-console.log("Vivek")
+const a=12345
+let b="vivek"
+var c="vik@gmail.com"
+
+console.table([a,b,c])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
