3f4e5d3b5178dcb596822e22d82d710229df5426
VivekRajbhar23 <114468572+VivekRajbhar23@users.noreply.github.com>
2024-12-27 07:25:40 +0000

    stack&heap

---
+++ .devcontainer/01_basics_of_js/stack&heap.js
@@ -0,0 +1,9 @@
+
+
+//Stack(Premitive), Heap(Non-Premitive(reference))
+
+//whenever we declare a premitive datatype ir will store in Stack memory and whenerver we will change the value it change in the copy of the
+// value not original value.
+
+//whenever we declare a non-premitive datatype ir will store in heap memory and whenerver we will change the value it change in the original of the
+// value .
