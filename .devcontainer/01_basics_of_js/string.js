3cf8120d00e9bf179d900ca5affd8db1e553fa51
VivekRajbhar23 <114468572+VivekRajbhar23@users.noreply.github.com>
2024-12-29 05:29:32 +0000

    strings in js

---
+++ .devcontainer/01_basics_of_js/string.js
@@ -0,0 +1,47 @@
+
+const name = "Vivek"
+const repoCount = 50
+
+//console.log(name + repocount + "value")   we should not use this old way to write code
+
+// console.log(`Hello my is ${name.toUpperCase()} and my repoCount is ${repoCount}`);
+
+// const gameName = new String('Vivek Rajbhar');
+
+// console.log(gameName[0]);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf('e'));
+
+// const newString = gameName.substring(0, 4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 3);
+// console.log(anotherString);
+
+const newStringOne= "     vivek    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://google.com/vivek%20rajbhar";
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sunder'));
+
+console.log(url.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
