38219102af9876510b8af4c9c5bd7d9f077c8132
VivekRajbhar23 <114468572+VivekRajbhar23@users.noreply.github.com>
2024-12-30 06:53:35 +0000

    date & time

---
+++ .devcontainer/01_basics_of_js/date&time.js
@@ -0,0 +1,30 @@
+
+//Date & Time
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23);
+//console.log(myCreatedDate.toDateString());
+
+//let myCreatedDate = new Date("2024-01-14");
+//console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate = new Date("01-14-2024");
+// console.log(myCreatedDate);
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Date.now());
+
+
+
+
+
+
